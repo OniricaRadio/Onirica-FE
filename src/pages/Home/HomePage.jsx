d5734faaa68c3c5fad0a9757968f7212ac2894fd
Karisha:) <krshmelendez@gmail.com>
2025-05-14 23:36:40 +0200

    created homePage body + responsive

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,14 +1,44 @@
-import styles from "./HomePage.module.css"
+import { useState, useEffect } from "react";
+import styles from "./HomePage.module.css";
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
 
 function HomePage() {
+    const gifList = [
+        "/public/img/Art Fire GIF by Daniel Barreto.gif",
+        "/public/img/mirror GIF by ewanjonesmorris.gif",
+        "/public/img/Tree Mirror GIF by Blanca Viñas.gif",
+        "/public/img/Photography Reflection GIF by John Fogarty.gif"
+    ];
+
+    const [currentGif, setCurrentGif] = useState(gifList[0]);
+
+    useEffect(() => {
+        const changeGif = () => {
+            const nextGif = gifList[Math.floor(Math.random() * gifList.length)];
+            setCurrentGif(nextGif);
+        };
+
+        changeGif(); 
+        const interval = setInterval(changeGif, 180000); 
+
+        return () => clearInterval(interval); 
+    }, []);
+
     return (
-        <div className={styles.bodyContainer}>
+        <>
             <Header />
-            <Footer/>
-        </div>
+            <div className={styles.bodyContainer}>
+                <img
+                    src={currentGif}
+                    alt="Background animation"
+                    className={styles.backgroundImage}
+                />
+                <h1 className={styles.title}>ONiriCA</h1>
+            </div>
+            <Footer />
+        </>
     );
 }
 
-export default HomePage
+export default HomePage;
