86b810104f7a17addef9170c1095af14363a2c57
Karisha:) <krshmelendez@gmail.com>
2025-05-14 15:58:45 +0200

    fisrt commit

---
+++ src/application/Router.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import App from "../App";
+
+
+function Router(){
+    return(
+<BrowserRouter>
+
+<App></App>
+
+</BrowserRouter>
+    );
+}
+
+export default Router;
