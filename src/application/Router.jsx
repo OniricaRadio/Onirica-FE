4c034c558895f7bff4322da961e69a1c3a771ddf
Karisha:) <krshmelendez@gmail.com>
2025-05-17 21:35:35 +0200

    created BookReading page

--- src/application/Router.jsx
+++ src/application/Router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/Home/HomePage";
 import AboutPage from "../pages/About/AboutPage"
 import LoginPage from "../pages/Login/Login";
+import BookingPage from "../pages/BookReading/BookReading"
 
 function Router() {
     return (
@@ -10,6 +11,7 @@ function Router() {
                 <Route path="/" element={<HomePage />} />
                 <Route path="/about" element={<AboutPage />} />
                 <Route path="/login" element={<LoginPage />} />
+                <Route path="/booking" element={<BookingPage />} />
             </Routes>
         </BrowserRouter>
     );
